c2653358a5a5f305285953d63c639337b0124450
radupruna <rp14g11@soton.ac.uk>
2015-03-16 19:26:39 +0000

    first

--- Chartist.py
+++ Chartist.py
@@ -1,1 +1,11 @@
 __author__ = 'Radu'
+import numpy
+
+class Chartist(object):
+    chi = 0.18
+    sigma_c = 0.79
+
+    def getDemand(self,p_0,p_t):
+        epsilon_c = numpy.random.normal(0,self.sigma_c)
+        demand = self.chi * (p_t - p_0) * epsilon_c
+        return demand
