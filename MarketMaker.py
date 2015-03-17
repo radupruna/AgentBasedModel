77ae311fc9b48157f34d58c5baf903e9c723acdf
radupruna <rp14g11@soton.ac.uk>
2015-03-17 14:26:21 +0000

    update

--- MarketMaker.py
+++ MarketMaker.py
@@ -1,6 +1,23 @@
 __author__ = 'Radu'
+import Fundamentalist
+import Chartist
+import SwitchingMechanism
+
 class MarketMaker:
-    p_t = []
-    def __init__(self,p0,p1):
-        self.p_t[0] = p0
-        self.p_t[1] = p1
+    pt = []
+    fund = Fundamentalist
+    chart = Chartist
+    sm = SwitchingMechanism
+    nf=sm.nf
+    nc=sm.nc
+
+    def __init__(self,pf,p0,p1,n_f, n_c):
+        self.pf = pf
+        self.pt[0] = p0
+        self.pt[1] = p1
+        self.nf[0] = n_f
+        self.nc[0] =n_c
+        fund = Fundamentalist(pf)
+        chart = Chartist(p0, p1)
+        sm = SwitchingMechanism(n_f,n_c)
+
