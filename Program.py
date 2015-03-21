c0f5be75ed6e68b57d1638b0043ca82e81e793c8
radupruna <rp14g11@soton.ac.uk>
2015-03-21 19:38:56 +0000

    update

--- Program.py
+++ Program.py
@@ -1,7 +1,7 @@
 __author__ = 'Radu'
 import numpy
 import matplotlib.pyplot as plt
-
+import scipy.stats as sts
 
 class Chartist:
     chi = 1.50
@@ -28,39 +28,39 @@ class Fundamentalist:
 
 class MarketMaker:
     pf = 0  # Fundamental price
-    pt = []  # Price series
+    price_t = []  # Price series
     nf = []  # Market Fraction of Fundamentalists
     nc = []  # Market Fraction of Technical Analysts
-    xt = []  # Majority index
+    x_t = []  # Majority index
     df = []  # Demands of Fundamentalists
     dc = []  # Demands of Chartists
     attract = []  # Attractiveness Levels
-    rt = []  # Returns
+    return_t = []  # Returns
 
     fund = Fundamentalist
     chart = Chartist
 
     def __init__(self, p_0, p_1, nf_0, nc_0):
-        self.pt.append(p_0)
-        self.pt.append(p_1)
+        self.price_t.append(p_0)
+        self.price_t.append(p_1)
         self.nf.append(nf_0)
         self.nc.append(nc_0)
-        self.xt.append(nf_0 - nc_0)
-        self.rt.append(100 * (p_1 - p_0))
+        self.x_t.append(nf_0 - nc_0)
+        self.return_t.append(100 * (p_1 - p_0))
 
         self.fund = Fundamentalist()
         self.chart = Chartist()
 
     def update_demands(self):
-        self.df.append(self.fund.demand(self.pt))
-        self.dc.append(self.chart.demand(self.pt))
+        self.df.append(self.fund.demand(self.price_t))
+        self.dc.append(self.chart.demand(self.price_t))
 
     @staticmethod
     def get_attractiveness(p_f, x_t, p_t):
         alpha_0 = -0.336
         alpha_n = 1.839
         alpha_p = 19.671
-        a_t = alpha_0 + alpha_n * x_t + alpha_p * (p_t - p_f)
+        a_t = alpha_0 + alpha_n * x_t + alpha_p * (p_t - p_f) ** 2
         return a_t
 
     def update_fractions(self, a_t):
@@ -69,17 +69,60 @@ class MarketMaker:
         n_c = 1 - n_f
         self.nf.append(n_f)
         self.nc.append(n_c)
-        self.xt.append(n_f - n_c)
+        self.x_t.append(n_f - n_c)
 
     def update_price(self):
         mu = 0.01
         self.update_demands()
-        a = self.get_attractiveness(self.pf, self.xt[-1], self.pt[-1])
+        a = self.get_attractiveness(self.pf, self.x_t[-1], self.price_t[-1])
         self.attract.append(a)
         self.update_fractions(a)
-        price = self.pt[-1] + mu * (self.dc[-1] * self.nc[-1] + self.df[-1] * self.nf[-1])
-        self.pt.append(price)
-        self.rt.append(100 * (self.pt[-1] - self.pt[-2]))
+        price = self.price_t[-1] + mu * (self.dc[-1] * self.nc[-1] + self.df[-1] * self.nf[-1])
+        self.price_t.append(price)
+        self.return_t.append(100 * (self.price_t[-1] - self.price_t[-2]))
 
 
-MM = MarketMaker(0.5, 0.5, 0.5, 0.5)
+MM = MarketMaker(0, 0, 0.5, 0.5)
+
+for i in range(3000):
+    MM.update_price()
+
+print ('Kurtosis: ',sts.kurtosis(MM.price_t, fisher=False))
+print ('Skewness: ',sts.skew(MM.price_t))
+
+plt.figure()
+plt.plot(MM.price_t)
+plt.axhline(0, color='black', ls='dotted', lw=1)
+plt.ylabel('log price')
+plt.xlabel('time')
+plt.title("Price Series")
+
+plt.figure()
+plt.plot(MM.x_t)
+plt.axhline(0, color='black', ls='dotted', lw=1)
+plt.ylabel('majority index')
+plt.xlabel('time')
+plt.title("Majority Index")
+
+plt.figure()
+plt.plot(MM.return_t)
+plt.title("Returns")
+plt.ylabel('return')
+plt.xlabel('time')
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(MM.price_t)
+plt.axhline(0, color='black', ls='dotted', lw=1)
+plt.title('log price')
+plt.subplot(2,1,2)
+plt.plot(MM.x_t)
+plt.axhline(0, color='black', ls='dotted', lw=1)
+plt.title('majority index')
+
+print('mean price= ', numpy.mean(MM.price_t))
+print('mean return= ',numpy.mean(MM.return_t))
+print('mean majority index= ',numpy.mean(MM.x_t))
+plt.show()
+
+
